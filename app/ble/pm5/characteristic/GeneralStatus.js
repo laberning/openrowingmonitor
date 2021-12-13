4dc29e50788cbfde49fbae063cc7a61de576c964
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2021-12-13 21:20:51 +0100

    Fixed a bug
    Lint saved the day, and prevented a logic error....

--- app/ble/pm5/characteristic/GeneralStatus.js
+++ app/ble/pm5/characteristic/GeneralStatus.js
@@ -46,7 +46,7 @@ export default class GeneralStatus extends bleno.Characteristic {
       // intervalType: UInt8 will always use 255 (NONE)
       bufferBuilder.writeUInt8(255)
       // workoutState: UInt8 0 WAITTOBEGIN, 1 WORKOUTROW, 10 WORKOUTEND
-      bufferBuilder.writeUInt8(data.sessionStatus === 'rowing' ? 1 : (data.sessionStatus = 'waitingForStart' ? 0 : 10))
+      bufferBuilder.writeUInt8(data.sessionStatus === 'rowing' ? 1 : (data.sessionStatus === 'waitingForStart' ? 0 : 10))
       // rowingState: UInt8 0 INACTIVE, 1 ACTIVE
       bufferBuilder.writeUInt8(data.sessionStatus === 'rowing' ? 1 : 0)
       // strokeState: UInt8 2 DRIVING, 4 RECOVERY
