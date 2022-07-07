51db66c522a046043ddf43b59e82212da58c278c
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2022-07-07 09:36:03 +0200

    Update StreamFilter.test.js

--- app/engine/utils/StreamFilter.test.js
+++ app/engine/utils/StreamFilter.test.js
@@ -5,40 +5,41 @@
 import { test } from 'uvu'
 import * as assert from 'uvu/assert'
 
-import { createMovingAverager } from './MovingAverager.js'
+import { createStreamFilter } from './StreamFilter.js'
 
 test('average should be initValue on empty dataset', () => {
-  const movingAverager = createMovingAverager(10, 5.5)
-  assert.is(movingAverager.getAverage(), 5.5)
+  const datapoints = createStreamFilter(3, 5.5)
+  assert.is(datapoints.clean(), 5.5)
 })
 
 test('an averager of length 1 should return the last added value', () => {
-  const movingAverager = createMovingAverager(1, 3)
-  movingAverager.pushValue(9)
-  assert.is(movingAverager.getAverage(), 9)
+  const datapoints = createStreamFilter(3, 5.5)
+  datapoints.push(9)
+  assert.is(datapoints.clean(), 9)
 })
 
-test('an averager of length 2 should return average of last 2 added elements', () => {
-  const movingAverager = createMovingAverager(2, 3)
-  movingAverager.pushValue(9)
-  movingAverager.pushValue(4)
-  assert.is(movingAverager.getAverage(), 6.5)
+test('a median of length 2 should return average of last 2 added elements', () => {
+  const datapoints = createStreamFilter(3, 5.5)
+  datapoints.push(9)
+  datapoints.push(4)
+  assert.is(datapoints.clean(), 6.5)
+})
+
+test('a median of three values should deliver the middle element', () => {
+  const datapoints = createStreamFilter(3, 5.5)
+  datapoints.push(9)
+  datapoints.push(4)
+  datapoints.push(3)
+  assert.is(datapoints.clean(), 4)
 })
 
 test('elements outside of range should not be considered', () => {
-  const movingAverager = createMovingAverager(2, 3)
-  movingAverager.pushValue(9)
-  movingAverager.pushValue(4)
-  movingAverager.pushValue(3)
-  assert.is(movingAverager.getAverage(), 3.5)
-})
-
-test('replacing the last element should work as expected', () => {
-  const movingAverager = createMovingAverager(2, 3)
-  movingAverager.pushValue(9)
-  movingAverager.pushValue(5)
-  movingAverager.replaceLastPushedValue(12)
-  assert.is(movingAverager.getAverage(), 10.5)
+  const datapoints = createStreamFilter(3, 5.5)
+  datapoints.push(9)
+  datapoints.push(4)
+  datapoints.push(3)
+  datapoints.push(1)
+  assert.is(datapoints.clean(), 3)
 })
 
 test.run()
