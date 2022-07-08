74f36b307b42b8e431d2567d06036fab7d02efe6
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2022-07-08 18:02:17 +0200

    Create Series.test.js

---
+++ app/engine/utils/Series.test.js
@@ -0,0 +1,239 @@
+'use strict'
+/*
+  Open Rowing Monitor, https://github.com/laberning/openrowingmonitor
+*/
+import { test } from 'uvu'
+import * as assert from 'uvu/assert'
+
+import { createSeries } from './Series.js'
+
+const dataSeries = createSeries(3)
+
+test('Length should be 0 on empty dataset', () => {
+  assert.is(dataSeries.length(), 0)
+})
+
+test('atSeriesBegin should be 0 on empty dataset', () => {
+  assert.is(dataSeries.atSeriesBegin(), 0)
+})
+
+test('atSeriesEnd should be 0 on empty dataset', () => {
+  assert.is(dataSeries.atSeriesEnd(), 0)
+})
+
+test('numberOfValuesAbove(0) should be 0 on empty dataset', () => {
+  assert.is(dataSeries.numberOfValuesAbove(0), 0)
+})
+
+test('numberOfValuesEqualOrBelow(0) should be 0 on empty dataset', () => {
+  assert.is(dataSeries.numberOfValuesEqualOrBelow(0), 0)
+})
+
+test('sum should be 0 on empty dataset', () => {
+  assert.is(dataSeries.sum(), 0)
+})
+
+test('average should be 0 on empty dataset', () => {
+  assert.is(dataSeries.average(), 0)
+})
+
+test('median should be 0 on empty dataset', () => {
+  assert.is(dataSeries.median(), 0)
+})
+
+dataSeries.push(9)
+// dataseries = [9]
+
+test('Length should be 1 after single push(9)', () => {
+  assert.is(dataSeries.length(), 1)
+})
+
+test('atSeriesBegin should be 9 after single push(9)', () => {
+  assert.is(dataSeries.atSeriesBegin(), 9)
+})
+
+test('atSeriesEnd should be 9 after single push(9)', () => {
+  assert.is(dataSeries.atSeriesEnd(), 0)
+})
+
+test('numberOfValuesAbove(0) should be 1 after single push(9)', () => {
+  assert.is(dataSeries.numberOfValuesAbove(0), 1)
+})
+
+test('numberOfValuesEqualOrBelow(0) should remain 0 after single push(9)', () => {
+  assert.is(dataSeries.numberOfValuesEqualOrBelow(0), 0)
+})
+
+test('numberOfValuesEqualOrBelow(10) should be 1 after single push(9)', () => {
+  assert.is(dataSeries.numberOfValuesEqualOrBelow(10), 1)
+})
+
+test('sum should be 9 after single push(9)', () => {
+  assert.is(dataSeries.sum(), 9)
+})
+
+test('average should be 9 after single push(9)', () => {
+  assert.is(dataSeries.average(), 9)
+})
+
+test('median should be 9 after single push(9)', () => {
+  assert.is(dataSeries.median(), 9)
+})
+
+dataSeries.push(3)
+// dataseries = [9,3]
+
+test('Length should be 2 after second push', () => {
+  assert.is(dataSeries.length(), 2)
+})
+
+test('atSeriesBegin should remain 9 after second push', () => {
+  assert.is(dataSeries.atSeriesBegin(), 9)
+})
+
+test('atSeriesEnd should be 3 after second push', () => {
+  assert.is(dataSeries.atSeriesEnd(), 3)
+})
+
+test('numberOfValuesAbove(0) should be 2 after second push', () => {
+  assert.is(dataSeries.numberOfValuesAbove(0), 2)
+})
+
+test('numberOfValuesEqualOrBelow(0) should remain 0 after second push', () => {
+  assert.is(dataSeries.numberOfValuesEqualOrBelow(0), 0)
+})
+
+test('numberOfValuesEqualOrBelow(10) should be 2 after second push', () => {
+  assert.is(dataSeries.numberOfValuesEqualOrBelow(10), 2)
+})
+
+test('sum should be 12 after second push', () => {
+  assert.is(dataSeries.sum(), 12)
+})
+
+test('average should be 6 after second push', () => {
+  assert.is(dataSeries.average(), 6)
+})
+
+test('median should be 6 after second push', () => {
+  assert.is(dataSeries.median(), 6)
+})
+
+dataSeries.push(6)
+// dataseries = [9,3,6]
+
+test('Length should be 3 after third push', () => {
+  assert.is(dataSeries.length(), 3)
+})
+
+test('atSeriesBegin should remain 9 after third push', () => {
+  assert.is(dataSeries.atSeriesBegin(), 9)
+})
+
+test('atSeriesEnd should be 6 after third push', () => {
+  assert.is(dataSeries.atSeriesEnd(), 6)
+})
+
+test('numberOfValuesAbove(0) should be 3 after third push', () => {
+  assert.is(dataSeries.numberOfValuesAbove(0), 3)
+})
+
+test('numberOfValuesEqualOrBelow(0) should remain 0 after third push', () => {
+  assert.is(dataSeries.numberOfValuesEqualOrBelow(0), 0)
+})
+
+test('numberOfValuesEqualOrBelow(10) should be 3 after third push', () => {
+  assert.is(dataSeries.numberOfValuesEqualOrBelow(10), 3)
+})
+
+test('sum should be 18 after third push', () => {
+  assert.is(dataSeries.sum(), 6)
+})
+
+test('average should be 6 after third push', () => {
+  assert.is(dataSeries.average(), 6)
+})
+
+test('median should be 6 after third push', () => {
+  assert.is(dataSeries.median(), 6)
+})
+
+dataSeries.push(12)
+// dataseries = [3,6,12]
+
+test('Length should remain 3 after fourth push', () => {
+  assert.is(dataSeries.length(), 3)
+})
+
+test('atSeriesBegin should turn to 3 after fourth push', () => {
+  assert.is(dataSeries.atSeriesBegin(), 3)
+})
+
+test('atSeriesEnd should be 12 after fourth push', () => {
+  assert.is(dataSeries.atSeriesEnd(), 12)
+})
+
+test('numberOfValuesAbove(0) should be 3 after fourth push', () => {
+  assert.is(dataSeries.numberOfValuesAbove(0), 3)
+})
+
+test('numberOfValuesEqualOrBelow(0) should remain 0 after fourth push', () => {
+  assert.is(dataSeries.numberOfValuesEqualOrBelow(0), 0)
+})
+
+test('numberOfValuesEqualOrBelow(10) should be 2 after fourth push', () => {
+  assert.is(dataSeries.numberOfValuesEqualOrBelow(10), 2)
+})
+
+test('sum should be 21 after fourth push', () => {
+  assert.is(dataSeries.sum(), 21)
+})
+
+test('average should be 6 after fourth push', () => {
+  assert.is(dataSeries.average(), 7)
+})
+
+test('median should remain 6 after fourth push', () => {
+  assert.is(dataSeries.median(), 6)
+})
+
+dataSeries.push(-3)
+// dataseries = [6,12,-3]
+
+test('Length should remain 3 after fifth push', () => {
+  assert.is(dataSeries.length(), 3)
+})
+
+test('atSeriesBegin should turn to 6 after fifth push', () => {
+  assert.is(dataSeries.atSeriesBegin(), 6)
+})
+
+test('atSeriesEnd should be -3 after fifth push', () => {
+  assert.is(dataSeries.atSeriesEnd(), -3)
+})
+
+test('numberOfValuesAbove(0) should be 2 after fifth push', () => {
+  assert.is(dataSeries.numberOfValuesAbove(0), 2)
+})
+
+test('numberOfValuesEqualOrBelow(0) should be 1 after fifth push', () => {
+  assert.is(dataSeries.numberOfValuesEqualOrBelow(0), 1)
+})
+
+test('numberOfValuesEqualOrBelow(10) should remain 2 after fifth push', () => {
+  assert.is(dataSeries.numberOfValuesEqualOrBelow(10), 2)
+})
+
+test('sum should be 15 after fifth push', () => {
+  assert.is(dataSeries.sum(), 15)
+})
+
+test('average should be 5 after fifth push', () => {
+  assert.is(dataSeries.average(), 5)
+})
+
+test('median should remain 6 after fifth push', () => {
+  assert.is(dataSeries.median(), 6)
+})
+
+test.run()
