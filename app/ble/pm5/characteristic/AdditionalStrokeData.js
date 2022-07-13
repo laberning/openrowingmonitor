8b689b648d09f4b91198666a96d5c5fefb385538
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2022-07-13 21:57:59 +0200

    Update AdditionalStrokeData.js

--- app/ble/pm5/characteristic/AdditionalStrokeData.js
+++ app/ble/pm5/characteristic/AdditionalStrokeData.js
@@ -38,13 +38,13 @@ export default class AdditionalStrokeData extends bleno.Characteristic {
     if (this._updateValueCallback || this._multiplexedCharacteristic.centralSubscribed()) {
       const bufferBuilder = new BufferBuilder()
       // elapsedTime: UInt24LE in 0.01 sec
-      bufferBuilder.writeUInt24LE(Math.round(data.durationTotal * 100))
+      bufferBuilder.writeUInt24LE(Math.round(data.totalMovingTime * 100))
       // strokePower: UInt16LE in watts
-      bufferBuilder.writeUInt16LE(Math.round(data.power))
+      bufferBuilder.writeUInt16LE(Math.round(data.cyclePower))
       // strokeCalories: UInt16LE in cal
-      bufferBuilder.writeUInt16LE(0)
+      bufferBuilder.writeUInt16LE(Math.round(data.strokeCalories * 1000))
       // strokeCount: UInt16LE
-      bufferBuilder.writeUInt16LE(Math.round(data.strokesTotal))
+      bufferBuilder.writeUInt16LE(Math.round(data.totalNumberOfStrokes))
       // projectedWorkTime: UInt24LE in 1 sec
       bufferBuilder.writeUInt24LE(0)
       // projectedWorkDistance: UInt24LE in 1 m
