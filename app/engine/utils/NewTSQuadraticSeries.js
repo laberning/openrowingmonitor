5bf8f2c247a88ba014148078c0a469479276e1fb
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2022-07-31 21:57:22 +0200

    Update NewTSQuadraticSeries.js

--- app/engine/utils/NewTSQuadraticSeries.js
+++ app/engine/utils/NewTSQuadraticSeries.js
@@ -2,9 +2,9 @@
 /*
   Open Rowing Monitor, https://github.com/jaapvanekris/openrowingmonitor
 
-  The TSQuadraticSeries is a datatype that represents a 2nd polynomial (quadratic) Series.
-  It allows values to be retrieved (like a FiFo buffer, or Queue) but it also includes
-  a Quadratic Theil-Sen estimator (Regression method) to determine the slope of this timeseries.
+  The TSLinearSeries is a datatype that represents a Linear Series. It allows
+  values to be retrieved (like a FiFo buffer, or Queue) but it also includes
+  a Theil-Sen estimator Linear Regressor to determine the slope of this timeseries.
 
   At creation its length is determined. After it is filled, the oldest will be pushed
   out of the queue) automatically.
@@ -13,47 +13,76 @@
   array based curve fitting), which might be performed on a Pi zero
 
   This implementation uses concepts that are described here:
-  https://stats.stackexchange.com/questions/317777/theil-sen-estimator-for-polynomial
+  https://stats.stackexchange.com/questions/317777/theil-sen-estimator-for-polynomial,
+  https://math.stackexchange.com/questions/710750/find-a-second-degree-polynomial-that-goes-through-3-points
+  https://www.physicsforums.com/threads/quadratic-equation-from-3-points.404174/
 
   The array is ordered such that x[0] is the oldest, and x[x.length-1] is the youngest
 */
 
 import { createSeries } from './Series.js'
-import { createFullTSLinearSeries } from './FullTSLinearSeries.js'
+
+import loglevel from 'loglevel'
+const log = loglevel.getLogger('RowingEngine')
 
 function createTSQuadraticSeries (maxSeriesLength = 0) {
   const X = createSeries(maxSeriesLength)
   const Y = createSeries(maxSeriesLength)
-  const A = createFullTSLinearSeries(maxSeriesLength)
-  const B = createFullTSLinearSeries(maxSeriesLength)
+  const A = []
+  const B = createSeries()
   let _A = 0
   let _B = 0
 
   function push (x, y) {
     X.push(x)
     Y.push(y)
-    A.push(Math.pow(x, 2), y)
+
+    if (maxSeriesLength > 0 && A.length >= maxSeriesLength) {
+      // The maximum of the array has been reached, we have to create room
+      // in the 2D array by removing the first row from the table
+      removeFirstRow()
+    }
 
     // Invariant: the indices of the X and Y array now match up with the
     // row numbers of the A array. So, the A of (X[0],Y[0]) and (X[1],Y[1]
     // will be stored in A[0][.].
 
-    // Calculate the quadratic element of this new point
+    // Calculate the slopes of this new point
     if (X.length() > 2) {
-      _A = A.slope()
+      // There are at least two points in the X and Y arrays, so let's add the new datapoint
+      let i = 0
+      let result = 0
+      let mid = 0
+      while (i < X.length() - 2) {
+        mid = Math.floor((i + A.length) / 2)
+        result = calculateA(i, mid, A.length)
+        A[i].push(result)
+        i++
+      }
+      _A = Amedian()
     } else {
       _A = 0
     }
+
+    // Add an empty array at the end to store futurs results for the most recent points
+    A.push([])
 
     B.reset()
     if (X.length() > 2) {
       // There are at least two points in the X and Y arrays, so let's add the new datapoint
       let i = 0
-      while (i <= X.length() - 1) {
-        B.push(x, y - (_A * Math.pow(x, 2)))
+      let j = 0
+      let result = 0
+      while (i < X.length() - 1) {
+        j = i + 1
+        while (j < X.length() - 1) {
+          result = calculateB(i, j)
+          B.push(result)
+          j++
+        }
         i++
       }
-      _B = B.slope()
+      _B = B.median()
     } else {
       _B = 0
     }
@@ -176,8 +205,47 @@ function createTSQuadraticSeries (maxSeriesLength = 0) {
     return Y.series()
   }
 
+  function removeFirstRow () {
+    A.shift()
+  }
+
+  function calculateA (pointOne, pointTwo, pointThree) {
+    // For the underlying math, see https://math.stackexchange.com/questions/710750/find-a-second-degree-polynomial-that-goes-through-3-points
+    // and https://www.physicsforums.com/threads/quadratic-equation-from-3-points.404174/
+    if (pointOne < pointTwo && pointTwo < pointThree && X.get(pointOne) !== X.get(pointTwo) && X.get(pointTwo) !== X.get(pointThree) && X.get(pointOne) !== X.get(pointThree)) {
+      return (X.get(pointThree) * (Y.get(pointTwo) - Y.get(pointOne)) + X.get(pointTwo) * (Y.get(pointOne) - Y.get(pointThree)) + X.get(pointOne) * (Y.get(pointThree) - Y.get(pointTwo))) / ((X.get(pointOne) - X.get(pointTwo)) * (X.get(pointOne) - X.get(pointThree)) * (X.get(pointTwo) - X.get(pointThree)))
+    } else {
+      log.error('TS Quadratic Regressor, Division by zero prevented in CalculateA!')
+      return 0
+    }
+  }
+
+  function calculateB (pointOne, pointTwo) {
+    // For the underlying math, see https://math.stackexchange.com/questions/710750/find-a-second-degree-polynomial-that-goes-through-3-points
+    // and https://www.physicsforums.com/threads/quadratic-equation-from-3-points.404174/
+    if (pointOne < pointTwo && X.get(pointOne) !== X.get(pointTwo)) {
+      return ((Y.get(pointTwo) - _A * Math.pow(X.get(pointTwo), 2)) - (Y.get(pointOne) - _A * Math.pow(X.get(pointOne), 2))) / (X.get(pointTwo) - X.get(pointOne))
+    } else {
+      log.error('TS Quadratic Regressor, Division by zero prevented in CalculateB!')
+      return 0
+    }
+  }
+
   function calculateC (pointOne) {
+    // For the underlying math, see https://math.stackexchange.com/questions/710750/find-a-second-degree-polynomial-that-goes-through-3-points
+    // and https://www.physicsforums.com/threads/quadratic-equation-from-3-points.404174/
     return Y.get(pointOne) - _A * Math.pow(X.get(pointOne), 2) - _B * X.get(pointOne)
+  }
+
+  function Amedian () {
+    if (A.length > 1) {
+      const sortedArray = [...A.flat()].sort((a, b) => a - b)
+      const mid = Math.floor(sortedArray.length / 2)
+      return (sortedArray.length % 2 !== 0 ? sortedArray[mid] : ((sortedArray[mid - 1] + sortedArray[mid]) / 2))
+    } else {
+      log.error('TS Quadratic Regressor, Median calculation on empty dataset attempted!')
+      return 0
+    }
   }
 
   function reset () {
