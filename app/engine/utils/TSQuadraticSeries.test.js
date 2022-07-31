8a4bd383079f6720f6b5c2f1823ef06d256e1483
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2022-07-31 23:34:59 +0200

    Update TSQuadraticSeries.test.js

--- app/engine/utils/TSQuadraticSeries.test.js
+++ app/engine/utils/TSQuadraticSeries.test.js
@@ -11,7 +11,7 @@ import { createTSQuadraticSeries } from './TSQuadraticSeries.js'
 
 // ToDo: Test startup behaviour
 
-test('Quadratic Approximation on a perfect noisefree function', () => {
+test('Quadratic Approximation on a perfect noisefree function y = 2 * Math.pow(x, 2) + 2 * x + 2, 21 datapoints', () => {
   // Data based on 2 x^2 + 2 x + 2
   const dataSeries = createTSQuadraticSeries(21)
   dataSeries.push(-10, 182)
@@ -41,7 +41,40 @@ test('Quadratic Approximation on a perfect noisefree function', () => {
   // ToDo: Test after moving several points
 })
 
-test('Quadratic Approximation on a perfect noisefree function', () => {
+test('Quadratic Approximation on a perfect noisefree function y = 2 * Math.pow(x, 2) + 2 * x + 2, with 10 datapoints andsome data shifting', () => {
+  // Data based on 2 x^2 + 2 x + 2, split the dataset in two to see its behaviour when it is around the Vertex
+  const dataSeries = createTSQuadraticSeries(10)
+  dataSeries.push(-10, 182)
+  dataSeries.push(-9, 146)
+  dataSeries.push(-8, 114)
+  dataSeries.push(-7, 86)
+  dataSeries.push(-6, 62)
+  dataSeries.push(-5, 42)
+  dataSeries.push(-4, 26)
+  dataSeries.push(-3, 14) // Pi ;)
+  dataSeries.push(-2, 6)
+  dataSeries.push(-1, 2)
+  dataSeries.push(0, 2)
+  assert.ok(dataSeries.coefficientA() === 2, `coefficientA should be 2, is ${dataSeries.coefficientA()}`)
+  assert.ok(dataSeries.coefficientB() === 2, `coefficientB should be 2, is ${dataSeries.coefficientB()}`)
+  assert.ok(dataSeries.coefficientC() === 2, `coefficientC should be 2, is ${dataSeries.coefficientC()}`)
+  dataSeries.push(1, 6)
+  dataSeries.push(2, 14)
+  dataSeries.push(3, 26)
+  dataSeries.push(4, 42)
+  dataSeries.push(5, 62)
+  dataSeries.push(6, 86)
+  dataSeries.push(7, 114)
+  dataSeries.push(8, 146)
+  dataSeries.push(9, 182)
+  dataSeries.push(10, 222)
+  assert.ok(dataSeries.coefficientA() === 2, `coefficientA should be 2, is ${dataSeries.coefficientA()}`)
+  assert.ok(dataSeries.coefficientB() === 2, `coefficientB should be 2, is ${dataSeries.coefficientB()}`)
+  assert.ok(dataSeries.coefficientC() === 2, `coefficientC should be 2, is ${dataSeries.coefficientC()}`)
+  // ToDo: Test after moving several points
+})
+
+test('Quadratic Approximation on a perfect noisefree function y = 4 * Math.pow(x, 2) + 4 * x + 4', () => {
   // Data based on 4 x^2 + 4 x + 4
   const dataSeries = createTSQuadraticSeries(22)
   dataSeries.push(-11, 444)
@@ -101,7 +134,7 @@ test('Quadratic Approximation on a function with some noise (+/- 1)', () => {
   assert.ok(dataSeries.coefficientC() === 3, `coefficientC should be 3, is ${dataSeries.coefficientC()}`) // We get a 3 instead of 4, which is quite acceptable (especially since ORM ignores the C)
 })
 
-test('Quadratic TS Estimation should be decent for standard real-life example with some noise', () => {
+test('Quadratic TS Estimation should be decent for standard real-life example from MathBits with some noise', () => {
   // Data based on https://mathbits.com/MathBits/TISection/Statistics2/quadratic.html
   const dataSeries = createTSQuadraticSeries(13)
   dataSeries.push(10, 115.6)
@@ -117,14 +150,58 @@ test('Quadratic TS Estimation should be decent for standard real-life example wi
   dataSeries.push(58, 244.2)
   dataSeries.push(60, 231.4)
   dataSeries.push(64, 180.4)
-  assert.ok(dataSeries.coefficientA() === -0.17623095238095238, `coefficientA should be -0.17623095238095238, is ${dataSeries.coefficientA()}`) // In the example, the TI084 results in -0.1737141137, which we consider acceptably close
-  assert.ok(dataSeries.coefficientB() === 15.008079945054945, `coefficientB should be 15.008079945054945, is ${dataSeries.coefficientB()}`) // In the example, the TI084 results in 14.52117133, which we consider acceptably close
-  assert.ok(dataSeries.coefficientC() === -35.802545604395675, `coefficientC should be -35.802545604395675, is ${dataSeries.coefficientC()}`) // In the example, the TI084 results in -21.89774466, which we consider acceptably close
+  assert.ok(dataSeries.coefficientA() === -0.17623095238095243, `coefficientA should be -0.17623095238095243, is ${dataSeries.coefficientA()}`) // In the example, the TI084 results in -0.1737141137, which we consider acceptably close
+  assert.ok(dataSeries.coefficientB() === 15.008079945054948, `coefficientB should be 15.008079945054948, is ${dataSeries.coefficientB()}`) // In the example, the TI084 results in 14.52117133, which we consider acceptably close
+  assert.ok(dataSeries.coefficientC() === -35.80254560439562, `coefficientC should be -35.80254560439562, is ${dataSeries.coefficientC()}`) // In the example, the TI084 results in -21.89774466, which we consider acceptably close
 })
 
-// ToDo: Test based on https://www.varsitytutors.com/hotmath/hotmath_help/topics/quadratic-regression
+test('Quadratic TS Estimation should be decent for standard real-life example from VarsityTutors with some noise', () => {
+  // Test based on https://www.varsitytutors.com/hotmath/hotmath_help/topics/quadratic-regression
+  const dataSeries = createTSQuadraticSeries(7)
+  dataSeries.push(-3, 7.5)
+  dataSeries.push(-2, 3)
+  dataSeries.push(-1, 0.5)
+  dataSeries.push(0, 1)
+  dataSeries.push(1, 3)
+  dataSeries.push(2, 6)
+  dataSeries.push(3, 14)
+  assert.ok(dataSeries.coefficientA() === 1.0666666666666667, `coefficientA should be 1.0666666666666667, is ${dataSeries.coefficientA()}`) // The example results in 1.1071 for OLS, which we consider acceptably close
+  assert.ok(dataSeries.coefficientB() === 0.833333333333333, `coefficientB should be 0.833333333333333, is ${dataSeries.coefficientB()}`) // The example results in 1 for OLS, which we consider acceptably close
+  assert.ok(dataSeries.coefficientC() === 0.39999999999999947, `coefficientC should be 0.39999999999999947, is ${dataSeries.coefficientC()}`) // The example results in 0.5714 for OLS, which we consider acceptably close
+})
 
-// ToDo: Test based on https://www.thedatascientists.com/polynomial-regression/
+test('Quadratic TS Estimation should be decent for standard real-life example from TheDataScientist with some noise without the vertex being part of the dataset', () => {
+  // Test based on https://www.geo.fu-berlin.de/en/v/soga/Basics-of-statistics/Linear-Regression/Polynomial-Regression/Polynomial-Regression---An-example/index.html
+  const dataSeries = createTSQuadraticSeries(25)
+  dataSeries.push(0.001399613, -0.23436656)
+  dataSeries.push(0.971629779, 0.64689524)
+  dataSeries.push(0.579119475, -0.92635765)
+  dataSeries.push(0.335693937, 0.13000706)
+  dataSeries.push(0.736736086, -0.89294863)
+  dataSeries.push(0.492572335, 0.33854780)
+  dataSeries.push(0.737133774, -1.24171910)
+  dataSeries.push(0.563693769, -0.22523318)
+  dataSeries.push(0.877603280, -0.12962722)
+  dataSeries.push(0.141426545, 0.37632006)
+  dataSeries.push(0.307203910, 0.30299077)
+  dataSeries.push(0.024509308, -0.21162739)
+  dataSeries.push(0.843665029, -0.76468719)
+  dataSeries.push(0.771206067, -0.90455412)
+  dataSeries.push(0.149670258, 0.77097952)
+  dataSeries.push(0.359605608, 0.56466366)
+  dataSeries.push(0.049612895, 0.18897607)
+  dataSeries.push(0.409898906, 0.32531750)
+  dataSeries.push(0.935457898, -0.78703491)
+  dataSeries.push(0.149476207, 0.80585375)
+  dataSeries.push(0.234315216, 0.62944986)
+  dataSeries.push(0.455297119, 0.02353327)
+  dataSeries.push(0.102696671, 0.27621694)
+  dataSeries.push(0.715372314, -1.20379729)
+  dataSeries.push(0.681745393, -0.83059624)
+  assert.ok(dataSeries.coefficientA() === -3.1827927919154013, `coefficientA should be -3.1827927919154013, is ${dataSeries.coefficientA()}`)
+  assert.ok(dataSeries.coefficientB() === 1.634722701306206, `coefficientB should be 1.634722701306206, is ${dataSeries.coefficientB()}`)
+  assert.ok(dataSeries.coefficientC() === 0.11864001198418168, `coefficientC should be 0.11864001198418168, is ${dataSeries.coefficientC()}`)
+})
 
 // ToDo: Test after reset
 
