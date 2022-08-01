f14bf426bf0cdb88c0fe51f53b38c9fab44fbd4f
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2022-08-01 21:19:24 +0200

    Update NewerTSQuadraticSeries.js

--- app/engine/utils/NewerTSQuadraticSeries.js
+++ app/engine/utils/NewerTSQuadraticSeries.js
@@ -211,7 +211,7 @@ function createTSQuadraticSeries (maxSeriesLength = 0) {
     // For the underlying math, see https://math.stackexchange.com/questions/710750/find-a-second-degree-polynomial-that-goes-through-3-points
     // and https://www.physicsforums.com/threads/quadratic-equation-from-3-points.404174/
     if (pointOne < pointTwo && pointTwo < pointThree && X.get(pointOne) !== X.get(pointTwo) && X.get(pointTwo) !== X.get(pointThree) && X.get(pointOne) !== X.get(pointThree)) {
-      return (Math.pow(X.get(pointOne), 2)) * (Y.get(pointThree) - Y.get(pointTwo)) + X.get(pointOne) * (Y.get(pointOne) * X.get(pointTwo) - Y.get(pointOne) * X.get(pointThree)) + Math.pow(X.get(pointThree), 2)) * (Y.get(pointTwo) - Y.get(pointOne)) + X.get(pointTwo) * (Y.get(pointOne) * X.get(pointThree) - Y.get(pointThree) * X.get(pointTwo))) / ( Math.pow(X.get(pointOne), 2)) * (X.get(pointThree) - X.get(pointTwo)) + X.get(pointOne) * (Math.pow(X.get(pointTwo), 2)) - Math.pow(X.get(pointThree), 2))) + X.get(pointTwo) * X.get(pointThree) * (X.get(pointThree) - X.get(pointTwo)))
+      return (Math.pow(X.get(pointOne), 2) * (Y.get(pointThree) - Y.get(pointTwo)) + X.get(pointOne) * (Y.get(pointOne) * X.get(pointTwo) - Y.get(pointOne) * X.get(pointThree)) + Math.pow(X.get(pointThree), 2) * (Y.get(pointTwo) - Y.get(pointOne)) + X.get(pointTwo) * (Y.get(pointOne) * X.get(pointThree) - Y.get(pointThree) * X.get(pointTwo))) / ( Math.pow(X.get(pointOne), 2) * (X.get(pointThree) - X.get(pointTwo)) + X.get(pointOne) * (Math.pow(X.get(pointTwo), 2) - Math.pow(X.get(pointThree), 2)) + X.get(pointTwo) * X.get(pointThree) * (X.get(pointThree) - X.get(pointTwo)))
     } else {
       log.error('TS Quadratic Regressor, Division by zero prevented in CalculateB!')
       return 0
