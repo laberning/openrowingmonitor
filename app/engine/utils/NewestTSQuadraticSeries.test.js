87b3aea2401bc118b96a0dd3b43ded72e21d6e98
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2022-08-02 23:45:28 +0200

    Update NewestTSQuadraticSeries.test.js

--- app/engine/utils/NewestTSQuadraticSeries.test.js
+++ app/engine/utils/NewestTSQuadraticSeries.test.js
@@ -192,7 +192,7 @@ test('Quadratic Approximation on function y = 4 * Math.pow(x, 2) + 4 * x + 4, wi
   assert.ok(dataSeries.coefficientB() === -8, `coefficientB should be -8 after 4 datapoints, is ${dataSeries.coefficientB()}`)
   assert.ok(dataSeries.coefficientC() === -48.33333333333303, `coefficientC should be -48.33333333333303 after 4 datapoints, is ${dataSeries.coefficientC()}`)
   dataSeries.push(-7, 171)
-  assert.ok(dataSeries.coefficientA() === 4, `coefficientA should be 4 after 5 datapoints, is ${dataSeries.coefficientA()}`)
+  assert.ok(dataSeries.coefficientA() === 3.666666666666667, `coefficientA should be 3.666666666666667 after 5 datapoints, is ${dataSeries.coefficientA()}`)
   assert.ok(dataSeries.coefficientB() === 4, `coefficientB should be 4 after 5 datapoints, is ${dataSeries.coefficientB()}`)
   assert.ok(dataSeries.coefficientC() === 4, `coefficientC should be 4 after 5 datapoints, is ${dataSeries.coefficientC()}`)
   dataSeries.push(-6, 125)
