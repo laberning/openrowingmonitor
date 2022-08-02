bff699ee9f5ed148c3166542fe4d9e643052b3eb
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2022-08-02 17:45:22 +0200

    Update NewerTSQuadraticSeries.test.js

--- app/engine/utils/NewerTSQuadraticSeries.test.js
+++ app/engine/utils/NewerTSQuadraticSeries.test.js
@@ -181,7 +181,7 @@ test('Quadratic Approximation on a function with some noise (+/- 1) and spikes (
   assert.ok(dataSeries.coefficientB() === 3.6888888888888878, `coefficientB should be 3.6888888888888878 after 12 datapoints, is ${dataSeries.coefficientB()}`)
   assert.ok(dataSeries.coefficientC() === 3.322222222222215, `coefficientC should be 3.322222222222215 after 12 datapoints, is ${dataSeries.coefficientC()}`) // We get a 3 instead of 4, which is quite acceptable (especially since ORM ignores the C)
   dataSeries.push(1, 11)
-  assert.ok(dataSeries.coefficientA() === 3.92, `coefficientA should be 3.92 after 13 datapoints, is ${dataSeries.coefficientA()}`) // Coefficient A seems to take a hit anyway
+  assert.ok(dataSeries.coefficientA() === 3.9555555555555557, `coefficientA should be 3.9555555555555557 after 13 datapoints, is ${dataSeries.coefficientA()}`) // Coefficient A seems to take a hit anyway
   assert.ok(dataSeries.coefficientB() === 3.3599999999999968, `coefficientB should be 3.3599999999999968 after 13 datapoints, is ${dataSeries.coefficientB()}`) // Coefficient B seems to take a hit anyway
   assert.ok(dataSeries.coefficientC() === 3.719999999999988, `coefficientC should be 3.719999999999988 after 13 datapoints, is ${dataSeries.coefficientC()}`) // We get a 3.719999999999988 instead of 4, which is quite acceptable (especially since ORM ignores the C)
   dataSeries.push(2, 29)
