9e9dd3765a8628d4537a975ea2e0de97aaac594c
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2022-08-02 17:07:58 +0200

    Update NewerTSQuadraticSeries.js

--- app/engine/utils/NewerTSQuadraticSeries.js
+++ app/engine/utils/NewerTSQuadraticSeries.js
@@ -52,10 +52,8 @@ function createTSQuadraticSeries (maxSeriesLength = 0) {
       // There are at least two points in the X and Y arrays, so let's add the new datapoint
       let i = 0
       let result = 0
-      let mid = 0
       while (i < X.length() - 2) {
-        mid = Math.floor((i + A.length) / 2)
-        result = calculateA(i, mid, A.length)
+        result = calculateA(i, A.length)
         A[i].push(result)
         i++
       }
@@ -212,11 +210,17 @@ function createTSQuadraticSeries (maxSeriesLength = 0) {
     A.shift()
   }
 
-  function calculateA (pointOne, pointTwo, pointThree) {
+  function calculateA (pointOne, pointThree) {
     // For the underlying math, see https://math.stackexchange.com/questions/710750/find-a-second-degree-polynomial-that-goes-through-3-points
     // and https://www.physicsforums.com/threads/quadratic-equation-from-3-points.404174/
-    if (pointOne < pointTwo && pointTwo < pointThree && X.get(pointOne) !== X.get(pointTwo) && X.get(pointTwo) !== X.get(pointThree) && X.get(pointOne) !== X.get(pointThree)) {
-      return (X.get(pointOne) * (Y.get(pointTwo) - Y.get(pointThree)) + X.get(pointThree) * (Y.get(pointOne) - Y.get(pointTwo)) + X.get(pointTwo) * (Y.get(pointThree) - Y.get(pointOne))) / ((X.get(pointThree) - X.get(pointTwo)) * Math.pow(X.get(pointOne), 2) + X.get(pointOne) * (Math.pow(X.get(pointTwo), 2) - Math.pow(X.get(pointThree), 2)) + (X.get(pointTwo) * Math.pow(X.get(pointThree), 2) - X.get(pointThree) * Math.pow(X.get(pointTwo), 2)))
+    if ((pointOne + 1) < pointThree && X.get(pointOne) !== X.get(pointThree)) {
+      const pointTwo = Math.floor((i + A.length) / 2)
+      if (pointOne < pointTwo && pointTwo < pointThree && X.get(pointOne) !== X.get(pointTwo) && X.get(pointTwo) !== X.get(pointThree)) {
+        return (X.get(pointOne) * (Y.get(pointTwo) - Y.get(pointThree)) + X.get(pointThree) * (Y.get(pointOne) - Y.get(pointTwo)) + X.get(pointTwo) * (Y.get(pointThree) - Y.get(pointOne))) / ((X.get(pointThree) - X.get(pointTwo)) * Math.pow(X.get(pointOne), 2) + X.get(pointOne) * (Math.pow(X.get(pointTwo), 2) - Math.pow(X.get(pointThree), 2)) + (X.get(pointTwo) * Math.pow(X.get(pointThree), 2) - X.get(pointThree) * Math.pow(X.get(pointTwo), 2)))
+      } else {
+        log.error('TS Quadratic Regressor, Division by zero prevented in CalculateA!')
+        return 0
+      }
     } else {
       log.error('TS Quadratic Regressor, Division by zero prevented in CalculateA!')
       return 0
