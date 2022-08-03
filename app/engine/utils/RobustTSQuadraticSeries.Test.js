1c544a150f21a2d8e1fa38e1af56c2612ba8f43d
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2022-08-03 22:01:53 +0200

    Update RobustTSQuadraticSeries.Test.js

--- app/engine/utils/RobustTSQuadraticSeries.Test.js
+++ app/engine/utils/RobustTSQuadraticSeries.Test.js
@@ -8,7 +8,7 @@
 import { test } from 'uvu'
 import * as assert from 'uvu/assert'
 
-import { createTSQuadraticSeries } from './TSQuadraticSeries.js'
+import { createTSQuadraticSeries } from './RobustTSQuadraticSeries.js'
 
 test('Quadratic Approximation startup behaviour', () => {
   const dataSeries = createTSQuadraticSeries(10)
