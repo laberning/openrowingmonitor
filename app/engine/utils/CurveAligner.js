0c6cef2cae729943550ba55190cdbb06eed50dfe
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2022-08-28 22:09:12 +0200

    Create CurveAligner.js
    CurveAligner for cleaning up the (power, force and handle velocity) curves

---
+++ app/engine/utils/CurveAligner.js
@@ -0,0 +1,43 @@
+'use strict'
+/*
+  Open Rowing Monitor, https://github.com/laberning/openrowingmonitor
+
+  This keeps an array, for ForceMetrics, and cleans it up
+*/
+
+function createCurveAligner (minimumValue) {
+  let _lastCompleteCurve = []
+
+  function push (curve) {
+    // First, remove all unneccessary leading zero's
+    while (curve.length > 5 && (curve[0] < minimumValue || curve[1] < minimumValue || curve[2] < minimumValue || curve[3] < minimumValue || curve[4] < minimumValue)) {
+      curve.shift()
+    }
+
+    // Next, we clean up the trailing noise in the tail of the array
+    while (curve.length > 5 && (curve[curve.length - 1] < minimumValue || curve[curve.length - 2] < minimumValue || curve[curve.length - 3] < minimumValue || curve[curve.length - 4] < minimumValue || curve[curve.length - 5] < minimumValue)) {
+      curve.pop()
+    }
+    _lastCompleteCurve = Array.from(curve)
+  }
+
+  function lastCompleteCurve () {
+    if (_lastCompleteCurve.length > 0) {
+      return _lastCompleteCurve
+    } else {
+      return []
+    }
+  }
+
+  function reset () {
+    _lastCompleteCurve.splice(0, _lastCompleteCurve.length)
+  }
+
+  return {
+    push,
+    lastCompleteCurve,
+    reset
+  }
+}
+
+export { createCurveAligner }
