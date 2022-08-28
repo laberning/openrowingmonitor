75b035f5454be7c8cd837c20a410e30739567c6e
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2022-08-28 23:13:11 +0200

    Create curveMetrics.js
    Object to manage the specifics of curves

---
+++ app/engine/utils/curveMetrics.js
@@ -0,0 +1,76 @@
+'use strict'
+/*
+  Open Rowing Monitor, https://github.com/jaapvanekris/openrowingmonitor
+
+  This keeps an array, for all in-stroke metrics
+
+  Please note: The array contains flankLength + 1 values as it is in sync with the buffer of the FlyWheel
+  They are arranged that *Buffer[0] is the oldest, and *Buffer[currentLength] the youngest
+*/
+import { createSeries } from './Series.js'
+
+function createCurveMetrics (precission = 0) {
+  const _curve = createSeries()
+  let _max = 0
+  let totalInputXTime = 0
+  let totaltime = 0
+
+  function push (deltaTime, inputValue) {
+    // add the new dataPoint to the array, we have to move datapoints starting at the oldst ones
+    if (inputValue > 0) {
+      _curve.push(inputValue.toFixed(precission))
+      _max = Math.max(_max, inputValue)
+      totalInputXTime += deltaTime * inputValue
+      totaltime += deltaTime
+    } else {
+      // Let's skip negative and zero values with 0's as they are not relevant
+      _curve.push(0)
+    }
+  }
+
+  function peak () {
+    if (_max > 0) {
+      return _max
+    } else {
+      return 0
+    }
+  }
+
+  function average () {
+    if (totaltime > 0 && totalInputXTime > 0) {
+      return totalInputXTime / totaltime
+    } else {
+      return 0
+    }
+  }
+
+  function curve () {
+    if (_curve.length() > 0) {
+      return Array.from(_curve.series())
+    } else {
+      return []
+    }
+  }
+
+  function length () {
+    return _curve.length()
+  }
+
+  function reset () {
+    _curve.reset()
+    _max = 0
+    totalInputXTime = 0
+    totaltime = 0
+  }
+
+  return {
+    push,
+    peak,
+    average,
+    curve,
+    length,
+    reset
+  }
+}
+
+export { createCurveMetrics }
