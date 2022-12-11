bb9067a22ee8bf0f35c4e490367fad3b9f7367c3
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2022-12-12 00:19:16 +0100

    Update of the C2 testcase
    Update of the C2 testcase due to GPIO update

--- app/engine/Rower.test.js
+++ app/engine/Rower.test.js
@@ -439,7 +439,7 @@ test('A full session for a Concept2 RowErg should produce plausible results', as
   await replayRowingSession(rower.handleRotationImpulse, { filename: 'recordings/Concept2_RowErg_Session_2000meters.csv', realtime: false, loop: false })
 
   testTotalMovingTimeSinceStart(rower, 590.4201840000001)
-  testTotalLinearDistanceSinceStart(rower, 2031.2625865856846)
+  testTotalLinearDistanceSinceStart(rower, 2030.6574002852396)
   testTotalNumberOfStrokes(rower, 206)
   // As dragFactor isn't static, it should be updated
   testRecoveryDragFactor(rower, 80.81243632162892)
