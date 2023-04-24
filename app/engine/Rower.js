b5e439cd5009d64fdfac0b6a13b2334b2da0e076
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2023-04-24 10:26:39 +0200

    Fixed typo's

--- app/engine/Rower.js
+++ app/engine/Rower.js
@@ -73,7 +73,7 @@ function createRower (rowerSettings) {
         break
       case (_strokeState === 'Drive' && flywheel.isUnpowered()):
         // We seem to have lost power to the flywheel, but it is too early according to the settings. We stay in the Drive Phase
-        log.debug(`Time: ${flywheel.spinningTime().toFixed(4)} sec: Delta Time trend is upwards, suggests no power, but waiting for for drive phase length (${(flywheel.spinningTime() - drivePhaseStartTime).toFixed(4)} sec) to exceed minimumDriveTime (${rowerSettings.minimumDriveTime} sec)`)
+        log.debug(`Time: ${flywheel.spinningTime().toFixed(4)} sec: Delta Time trend is upwards, suggests no power, but waiting for drive phase length (${(flywheel.spinningTime() - drivePhaseStartTime).toFixed(4)} sec) to exceed minimumDriveTime (${rowerSettings.minimumDriveTime} sec)`)
         updateDrivePhase()
         break
       case (_strokeState === 'Drive'):
