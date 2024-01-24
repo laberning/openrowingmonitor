df7643e90c56b97e6075d33130a6598f1d85e4f4
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2024-01-24 17:20:27 +0100

    Adaptation to improved drag calculation algorithm

--- app/engine/Rower.test.js
+++ app/engine/Rower.test.js
@@ -273,7 +273,7 @@ test('Test behaviour for three perfect identical strokes, including settingling 
   testTotalNumberOfStrokes(rower, 3)
   testCycleDuration(rower, 0.3712367640000004)
   testCycleLinearDistance(rower, 1.3060154305498848)
-  testCycleLinearVelocity(rower, 4.76616864783023)
+  testCycleLinearVelocity(rower, 4.600477371517923)
   testCyclePower(rower, 303.1560556797095)
   testDriveDuration(rower, 0.2727572410000002)
   testDriveLinearDistance(rower, 1.3010125187490824)
